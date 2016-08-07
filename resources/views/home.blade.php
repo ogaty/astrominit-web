69e17e34a67de7c8052a8b7913cfee76c8f40e60
ogaty <trytodream1221@gmail.com>
2016-08-07 23:57:50 +0900

    microcosm-web start

---
+++ resources/views/home.blade.php
@@ -0,0 +1,109 @@
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<h1>覗いてみよう、君の中の宇宙を。</h1>
+<h2>ホロスコープ作成ソフト microcosm horoscope</h2>
+<div class="aboutbox">
+    <h3>ソフト紹介</h3>
+    <div class="aboutbox-text">
+        microcosm(マイクロコズム)は占星術士用ホロスコープ作成ソフトです。<br>
+        最新のシステムでも問題なく動作可能なこと、および他のソフトで不満に感じる点の解消をコンセプトとして開発しています。
+    </div>
+</div>
+<div class="featurebox clearfix">
+    <div class="featurebox-single">
+        <div class="featurebox-single__icon">
+        </div>
+        <div class="featurebox-single__title">
+            安心のswiss Ephemeris
+        </div>
+        <div class="featurebox-single__body">
+            様々なホロスコープ作成ソフトで使用されるswiss Ephemerisライブラリを使用しています。
+        </div>
+    </div>
+    <div class="featurebox-single">
+        <div class="featurebox-single__icon">
+        </div>
+        <div class="featurebox-single__title">
+            windows10対応
+        </div>
+        <div class="featurebox-single__body">
+            基本的に最新のシステムで開発しているため、最新システムでも不具合無く動作します。
+        </div>
+    </div>
+    <div class="featurebox-single">
+        <div class="featurebox-single__icon">
+        </div>
+        <div class="featurebox-single__title">
+            柔軟なオーブ設定
+        </div>
+        <div class="featurebox-single__body">
+            太陽オーブを8度、その他のオーブは6度といった設定も可能です。また、150度のオーブに関しては２種アスペクトと別に設定可能です。
+        </div>
+    </div>
+    <div class="featurebox-single">
+        <div class="featurebox-single__icon">
+        </div>
+        <div class="featurebox-single__title">
+            充実したインポート
+        </div>
+        <div class="featurebox-single__body">
+            別ソフトで作成したホロスコープファイルも読み込むことが可能です。
+        </div>
+    </div>
+    <div class="featurebox-single">
+        <div class="featurebox-single__icon">
+        </div>
+        <div class="featurebox-single__title">
+            CPSチャート表示
+        </div>
+        <div class="featurebox-single__body">
+            プライマリー・プログレッション、セカンダリー・プログレッションに加えCPS（月～金星までセカンダリー、太陽以降はプライマリー）表示に対応。
+        </div>
+    </div>
+    <div class="featurebox-single">
+        <div class="featurebox-single__icon">
+        </div>
+        <div class="featurebox-single__title">
+            google緯度経度検索
+        </div>
+        <div class="featurebox-single__body">
+            一覧から見つからない緯度経度もgoogleから検索して入力することができます。
+        </div>
+    </div>
+    <div class="featurebox-single">
+        <div class="featurebox-single__icon">
+        </div>
+        <div class="featurebox-single__title">
+            複数円対応
+        </div>
+        <div class="featurebox-single__body">
+            五重円まで様々な表示が可能です。
+        </div>
+    </div>
+</div>
+
+<div class="comparebox">
+    <table class="comparetable">
+        <tr>
+            <th></th>
+            <th>microcosmLite</th>
+            <th>microcosm</th>
+            <th>microcosmPro</th>
+            <th>microcosmMac</th>
+        </tr>
+        <tr>
+            <td>プラットフォーム</td>
+            <td>windows全般</td>
+            <td>windows10</td>
+            <td>windows10</td>
+            <td>MacOS X</td>
+        </tr>
+    </table>
+</div>
+
+</body>
+</html>
